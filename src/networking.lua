63e0864fef114e89e6fca1767ab56a288681bf57
Henrik Vester <henrik645@gmail.com>
2016-04-13 16:31:35 +0200

    Add networking module Responsible for all internal communication between computers in the system. NOTE: Requires the logging module

---
+++ src/networking.lua
@@ -0,0 +1,43 @@
+local settings_file_path = "networkSettings"
+os.loadApi("apis/logging")
+
+function initialise() -- initialises the settings file
+    if fs.exists(settings_file_path) then
+        settings_file = fs.open(settings_file_path)
+        settings = textutils.unserialize(fs.readAll()) -- takes care of the serialized settings file
+        fs.close(settings_file)
+    else
+        logging.error("networking: could not find settings file")
+    end
+end
+
+function send_message(modem, recipient, port, replyPort, data) -- sends a message on the network
+    modem.transmit(port, replyPort, {
+        recipient = recipient,
+        port = port,
+        replyPort = replyPort,
+        data = data
+    }
+    )
+end
+
+function receive_message(modem, port, timeout) -- takes care of receiving a message
+    modem.open(port)
+    timer = os.startTimer(timeout)
+    event, _, _, _, data, _ = os.pullEvent()
+    
+    if event == "timer" then
+        return
+    elseif event == "modem_message" then
+        handle_message(data)
+    end
+    os.cancelTimer(timer) -- makes sure the timer isn't running in the background
+end
+
+function handle_message(message) -- handles a message if you want to catch it yourself
+    if message.recipient == settings.nodeName then
+        return message.data
+    else
+        return false
+    end
+end
